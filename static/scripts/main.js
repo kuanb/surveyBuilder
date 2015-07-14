250bcde9859cecff294b29c2528a540aac0d5ada
Kuan Butts <kuanbutts@yahoo.com>
2015-07-14 15:12:00 -0400

    bower with sortable and modded index

---
+++ static/scripts/main.js
@@ -0,0 +1,21 @@
+var surveyBuilder = angular.module('surveyBuilderApp', ['ui.bootstrap', 'ui.sortable']);
+
+
+surveyBuilder.controller('surveyBuilder', function ($scope, $location, $http) {
+
+  console.log(flockSON);
+
+  $scope.flockSON = flockSON;
+
+  
+
+});
+
+
+
+
+
+
+
+
+
