1dcf34af319336699a8143270bd3af974ad6063c
Kuan Butts <kuanbutts@yahoo.com>
2015-07-15 17:02:43 -0400

    data bind to ques base and auto clean spaces on ques ID

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -18,16 +18,25 @@ surveyBuilder.controller('surveyBuilder', function ($scope, $location, $http) {
 
 
 surveyBuilder.controller('surveyController', function ($scope, $location, $http) {
+  $scope.questionKinds = ['IM', 'MC', 'CB', 'OL', ];
+  var questionBase = { ID: '', Text: 'SS', Kind: '', JumpID: '' };
+
   $scope.addNewChapter = function () {
     var title = document.getElementById('newChapter').value;
     document.getElementById('newChapter').value = '';
-    $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.push({ Chapter: { Questions: [], Title: title } });
+    $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.push({ Chapter: { Questions: [], Title: title,  } }); //newQuestion: questionBase
   };
   $scope.removeChapter = function (chapter) {
     $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.splice(chapter,1);
   }
   $scope.removeQuestion = function (chapter, question) {
     $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.Questions.splice(question, 1);
+  }
+
+
+
+  $scope.cleanQuestionID = function (chapter) {
+    if (chapter && chapter.newQuestion) { chapter.newQuestion.ID = chapter.newQuestion.ID.split(' ').join('_'); }
   }
 });
 
