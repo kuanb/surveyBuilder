6c5c0c217ad5ee796734191a57a76878c4d8aa47
Kuan Butts <kuanbutts@yahoo.com>
2015-07-15 19:38:31 -0400

    updated the add question component of indec

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -25,26 +25,37 @@ surveyBuilder.controller('surveyController', function ($scope, $location, $http)
     OT: 'Open Text',
     OL: 'Ordered List',
   };
-  var questionBase = { ID: '', Text: 'SS', Kind: '', JumpID: '' };
+  var questionBase = { ID: '', Text: '', Kind: '', JumpID: '' };
 
-  $scope.addNewChapter = function () {
+  $scope.addChapter = function () {
     var title = document.getElementById('newChapter').value;
     document.getElementById('newChapter').value = '';
-    $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.push({ Chapter: { Questions: [], Title: title,  } }); //newQuestion: questionBase
+    $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.push({ Chapter: { Questions: [], Title: title, newQuestion: questionBase} });
   };
   $scope.removeChapter = function (chapter) {
     $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.splice(chapter,1);
   }
+  $scope.addQuestion = function (chapter) {
+    var newQuestion = $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.newQuestion;
+    var ok = true;
+    if (newQuestion) {
+      if (!newQuestion.ID) { ok = false; }
+      if (!newQuestion.Text) { ok = false; }
+      if (!newQuestion.Kind) { ok = false; }
+      if (!newQuestion.JumpID) { newQuestion.JumpID = null; }
+      if (ok) { 
+        $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.Questions.push({Question: newQuestion}); 
+        $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.newQuestion = questionBase;
+      }
+    }
+    }
+
   $scope.removeQuestion = function (chapter, question) {
     $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.Questions.splice(question, 1);
   }
 
-
-
-  $scope.cleanQuestionID = function (chapter) {
-    if (chapter && chapter.newQuestion) { 
-      chapter.newQuestion.ID = chapter.newQuestion.ID.split(' ').join('_').split('.').join(''); 
-    }
+  $scope.cleanQuestionID = function (stringID) {
+    return stringID.split(' ').join('_').split('.').join(''); 
   }
 });
 
