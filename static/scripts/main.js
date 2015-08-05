994a4e9c0cd915356ba1aa78ed1f19c0025c4083
Kuan Butts <kuanbutts@yahoo.com>
2015-08-05 18:39:43 -0400

    implement DRY on surveyController

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -23,6 +23,7 @@ surveyBuilder.controller('surveyBuilder', function ($scope, $location, $http) {
   var fs  = new FT_pr(),
       res = fs.fSON.getJSON(inputString);
   $scope.flockSON = addSupportObjects(res);
+  $scope.currentTab = 'SurveyProject';
 
   // join survey operations
   $scope.vetQuesID = function (id) {
@@ -41,6 +42,11 @@ surveyBuilder.controller('surveyBuilder', function ($scope, $location, $http) {
       else { return true }
     } else { return false; }
   }
+  $scope.questionReady = function (question) {
+    return  $scope.vetQuesID(question.ID) && 
+            question.Text.length > 0 && 
+            question.Kind.length > 0 ? true : false;
+  }
 
   $scope.submit = function () {
     var submit = angular.copy($scope.flockSON);
@@ -57,66 +63,16 @@ surveyBuilder.controller('surveyBuilder', function ($scope, $location, $http) {
     });
     console.log(submit);
   }
+
+  $scope.tests = function () {
+    console.log($scope);
+  }
 });
 
 
 
 surveyBuilder.controller('surveyController', function ($scope, $location, $http) {
-  $scope.questionKinds = {
-    IM: { verbose: 'Photo',           other: false, answers: false },
-    MC: { verbose: 'Multiple Choice', other: true,  answers: true },
-    CB: { verbose: 'Checkbox',        other: true,  answers: true },
-    OT: { verbose: 'Open Text',       other: false, answers: false },
-    ON: { verbose: 'Open Number',     other: false, answers: false },
-    OL: { verbose: 'Ordered List',    other: false, answers: true },
-    LP: { verbose: 'Loop',            other: false, answers: true },
-  };
-  var answerBase = {Text: ''};
-  var questionBase = { ID: '', Text: '', Kind: '', JumpID: '', Other: null, Answers: [], newAnswer: answerBase };
 
-  $scope.addChapter = function () {
-    var title = document.getElementById('newSurveyChapter').value;
-    document.getElementById('newSurveyChapter').value = '';
-    $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.push({ Chapter: { Questions: [], Title: title, newQuestion: angular.copy(questionBase)} });
-  };
-  $scope.removeChapter = function (chapter) {
-    $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters.splice(chapter,1);
-  }
-  $scope.questionReady = function (question) {
-    return  $scope.vetQuesID(question.ID) && 
-            question.Text.length > 0 && 
-            question.Kind.length > 0 ? true : false;
-  }
-  $scope.addQuestion = function (chapter) {
-    var newQuestion = $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.newQuestion;
-    var ok = $scope.questionReady(newQuestion);
-    newQuestion.JumpID = newQuestion.JumpID ? newQuestion.JumpID : null;
-    if (ok) {
-      $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.Questions.push({Question: newQuestion}); 
-      $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.newQuestion = angular.copy(questionBase);
-    }
-  }
-
-  $scope.addAnswer = function (chapter) {
-    var newAnswer = $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.newQuestion.newAnswer;
-    if (newAnswer.length > 0) {
-      $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.newQuestion.Answers.push({Answer: newAnswer});
-      $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.newQuestion.newAnswer = angular.copy(answerBase);
-    }
-  }
-
-  $scope.removeQuestion = function (chapter, question) {
-    $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters[chapter].Chapter.Questions.splice(question, 1);
-  }
-
-  $scope.cleanQuestionID = function (stringID) {
-    return stringID.split(' ').join('_').split('.').join(''); 
-  }
-
-});
-
-
-surveyBuilder.controller('trackerController', function ($scope, $location, $http) {
   $scope.trackerPortion = 'StartSurvey';
   $scope.questionKinds = {
     IM: { verbose: 'Photo',           other: false, answers: false },
@@ -130,53 +86,47 @@ surveyBuilder.controller('trackerController', function ($scope, $location, $http
   var answerBase = {Text: ''};
   var questionBase = { ID: '', Text: '', Kind: '', JumpID: '', Other: null, Answers: [], newAnswer: answerBase };
 
+  var ref = function () {
+    var obj = $scope.flockSON.FlocktrackerProject;
+    var str;
+    if ($scope.currentTab=='SurveyProject') {
+      str = 'SurveyProject';
+    } else if ($scope.currentTab=='TrackerProject') {
+      str = 'TrackerProject.' + $scope.trackerPortion;
+    } else {
+      throw new Error('$scope not tracker or survey...')
+    }
+    return str.split('.').reduce(function(o, x) { return o[x] }, obj);
+  };
+
   $scope.addChapter = function () {
-    var title = document.getElementById('newTrackerChapter').value;
-    document.getElementById('newTrackerChapter').value = '';
-    $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters.push({ Chapter: { Questions: [], Title: title, newQuestion: angular.copy(questionBase)} });
+    var id = 'newChapter_' + $scope.currentTab;
+    var title = document.getElementById(id).value;
+    document.getElementById(id).value = '';
+    ref().Survey.Chapters.push({ Chapter: { Questions: [], Title: title, newQuestion: angular.copy(questionBase)} });
   };
   $scope.removeChapter = function (chapter) {
     $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters.splice(chapter,1);
-  }
-  $scope.questionReady = function (question) {
-    return  $scope.vetQuesID(question.ID) && 
-            question.Text.length > 0 && 
-            question.Kind.length > 0 ? true : false;
   }
   $scope.addQuestion = function (chapter) {
     var newQuestion = $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters[chapter].Chapter.newQuestion;
     var ok = $scope.questionReady(newQuestion);
     newQuestion.JumpID = newQuestion.JumpID ? newQuestion.JumpID : null;
     if (ok) {
-      $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters[chapter].Chapter.Questions.push({Question: newQuestion}); 
-      $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters[chapter].Chapter.newQuestion = angular.copy(questionBase);
+      ref().Survey.Chapters[chapter].Chapter.Questions.push({Question: newQuestion}); 
+      ref().Survey.Chapters[chapter].Chapter.newQuestion = angular.copy(questionBase);
     }
   }
-
-  $scope.vetQuesID = function (id) {
-    if (id && id.length > 0) {
-      var chapters = $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters;
-      var allIds = [];
-      chapters.forEach(function (chapter) {
-        chapter.Chapter.Questions.forEach(function (question) {
-          allIds.push(question.Question.ID);
-        })
-      });
-      if (allIds.indexOf(id) > -1) { return false } 
-      else { return true }
-    } else { return false; }
-  }
-
   $scope.addAnswer = function (chapter) {
-    var newAnswer = $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters[chapter].Chapter.newQuestion.newAnswer;
+    var newAnswer = ref().Survey.Chapters[chapter].Chapter.newQuestion.newAnswer;
     if (newAnswer.length > 0) {
-      $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters[chapter].Chapter.newQuestion.Answers.push({Answer: newAnswer});
-      $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters[chapter].Chapter.newQuestion.newAnswer = angular.copy(answerBase);
+      ref().Survey.Chapters[chapter].Chapter.newQuestion.Answers.push({Answer: newAnswer});
+      ref().Survey.Chapters[chapter].Chapter.newQuestion.newAnswer = angular.copy(answerBase);
     }
   }
 
   $scope.removeQuestion = function (chapter, question) {
-    $scope.flockSON.FlocktrackerProject.TrackerProject[$scope.trackerPortion].Survey.Chapters[chapter].Chapter.Questions.splice(question, 1);
+    ref().Survey.Chapters[chapter].Chapter.Questions.splice(question, 1);
   }
 
   $scope.cleanQuestionID = function (stringID) {
