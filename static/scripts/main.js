8beac8471b3ddb879a91f1b42229b270189341d3
Kuan Butts <kuanbutts@yahoo.com>
2015-08-10 17:42:15 -0400

    inst check when rm questions in case there is a ques id

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -21,17 +21,20 @@ surveyBuilder.controller('surveyBuilder', function ($scope, $location, $http) {
     return fs
   };
   var fs  = new FT_pr(),
-      res = fs.fSON.getJSON(inputString); console.log(res);
+      res = fs.fSON.getJSON(inputString);
   $scope.flockSON = addSupportObjects(res);
   $scope.currentTab = 'SurveyProject';
 
   // join survey operations
-  $scope.vetQuesID = function (id) {
-    if (id && id.length > 0) {
+  var getAllQuestions = function () {
       var surveyChaps = $scope.flockSON.FlocktrackerProject.SurveyProject.Survey.Chapters;
       var trackerChaps1 = $scope.flockSON.FlocktrackerProject.TrackerProject.StartSurvey.Survey.Chapters;
       var trackerChaps2 = $scope.flockSON.FlocktrackerProject.TrackerProject.EndSurvey.Survey.Chapters;
-      var chapters = surveyChaps.concat(trackerChaps1).concat(trackerChaps1);
+      return surveyChaps.concat(trackerChaps1).concat(trackerChaps1);
+  }
+  $scope.vetQuesID = function (id) {
+    if (id && id.length > 0) {
+      var chapters = getAllQuestions();
       var allIds = [];
       chapters.forEach(function (chapter) {
         chapter.Chapter.Questions.forEach(function (question) {
@@ -42,6 +45,41 @@ surveyBuilder.controller('surveyBuilder', function ($scope, $location, $http) {
         return false 
       } 
       else { return true }
+    } else { return false; }
+  }
+
+
+  $scope.checkJump = function (id) {
+    if (id && id.length > 0) {
+      var chapters = getAllQuestions();
+      allIds = {questions: [], answers: []};
+      chapters.forEach(function (chapter) {
+        chapter.Chapter.Questions.forEach(function (question) {
+          if (id == question.Question.JumpID) { 
+            allIds.questions.push(question) 
+          }
+          if (question.Question.Answers) {
+            question.Question.Answers.forEach(function (answer) {
+              if (id == answer.Answer.JumpID) { 
+                allIds.answers.push(answer) 
+              }
+            });
+          }
+        })
+      });
+      if (allIds.questions.length > 0 || allIds.answers.length > 0) { 
+        var string = [];
+        allIds.questions.forEach(function (question) {
+          string.push(' Question ' + question.Question.ID + ' (' + question.Question.Kind + ')');
+        });
+        allIds.answers.forEach(function (answer) {
+          string.push(' Answer ' + answer.Question.ID);
+        });
+        alert('The JumpIDs need to be changed before this can be removed: ' + string);
+        return false;
+      } else { 
+        return true;
+      }
     } else { return false; }
   }
 
@@ -170,7 +208,10 @@ surveyBuilder.controller('surveyController', function ($scope, $location, $http)
   }
 
   $scope.removeQuestion = function (chapter, question) {
-    ref().Survey.Chapters[chapter].Chapter.Questions.splice(question, 1);
+    var quesID = ref().Survey.Chapters[chapter].Chapter.Questions[question].Question.ID;
+    if ($scope.checkJump(quesID)) {
+      ref().Survey.Chapters[chapter].Chapter.Questions.splice(question, 1);
+    }
   }
 
 });
